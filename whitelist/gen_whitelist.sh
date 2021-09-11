d89ac5315756e5c409a1af63b3e4cbcd350eb029
bealanator <62661922+bealanator@users.noreply.github.com>
2021-09-10 19:13:11 -0500

    Added env bash hash-bang

--- whitelist/gen_whitelist.sh
+++ whitelist/gen_whitelist.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 wget https://discourse.pi-hole.net/t/commonly-whitelisted-domains/212 -O $(pwd)/whitelist/pihole_common_whitelist.html
 echo '#!/usr/bin/env bash' > $(pwd)/whitelist/common_whitelist_add.sh
 grep "<code class" $(pwd)/whitelist/pihole_common_whitelist.html >> $(pwd)/whitelist/common_whitelist_add.sh
