bdf4924b9c57826dc4b5a777bbac829b1494b5bd
Zachery Beal <bealanator@iCloud.com>
2021-09-10 19:08:31 -0500

    Changed paths in gen_whitelist to abslute paths

--- whitelist/gen_whitelist.sh
+++ whitelist/gen_whitelist.sh
@@ -1,6 +1,6 @@
-wget https://discourse.pi-hole.net/t/commonly-whitelisted-domains/212 -O pihole_common_whitelist.html
-echo '#!/usr/bin/env bash' > common_whitelist_add.sh
-grep "<code class" pihole_common_whitelist.html >> common_whitelist_add.sh
-sed -i 's/<pre><code class="lang-auto">//' common_whitelist_add.sh
-echo '#!/usr/bin/env bash' > common_whitelist_remove.sh
-cat common_whitelist_add.sh | sed -e 's/-w/-w -d/' >> common_whitelist_remove.sh
+wget https://discourse.pi-hole.net/t/commonly-whitelisted-domains/212 -O $(pwd)/whitelist/pihole_common_whitelist.html
+echo '#!/usr/bin/env bash' > $(pwd)/whitelist/common_whitelist_add.sh
+grep "<code class" $(pwd)/whitelist/pihole_common_whitelist.html >> $(pwd)/whitelist/common_whitelist_add.sh
+sed -i 's/<pre><code class="lang-auto">//' $(pwd)/whitelist/common_whitelist_add.sh
+echo '#!/usr/bin/env bash' > $(pwd)/whitelist/common_whitelist_remove.sh
+cat $(pwd)/whitelist/common_whitelist_add.sh | sed -e 's/-w/-w -d/' >> $(pwd)/whitelist/common_whitelist_remove.sh
